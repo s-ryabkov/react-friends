1722a8b962abc22c7fe961bbf78d312279cb8b44
Sergey Ryabkov <s.ryabkov@aimprosoft.com>
2017-08-16 19:22:50 +0300

    Auth: - add middleware for checking whether user is authenticated or not

--- server/routers.js
+++ server/routers.js
@@ -4,10 +4,12 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
 const Boom = require('boom');
+const _ = require('lodash');
 const app = express();
 const serveStatic = require('serve-static');
 
 const AuthRouter = require('./modules/auth/router');
+const AuthService = require('./modules/auth/service');
 
 /**
  * Middleware
@@ -15,6 +17,21 @@ const AuthRouter = require('./modules/auth/router');
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan('combined'));
+// check whether user is authenticated and add use data into request
+app.use((req, res, next) => {
+  const token = req.cookies.token;
+  if (_.isEmpty(token)) {
+    return next();
+  }
+
+  AuthService.getMe(token)
+    .then((user) => {
+      req.isAuthenticated = true;
+      req.principal = user;
+      next();
+    })
+    .catch((err) => next());
+});
 
 /**
  * Routes
